3b62d4f1b55790b3a833ae0af1ddd34d9b9e0cf4
wadajuntaro <wada-juntaro@g.ecc.u-tokyo.ac.jp>
2021-03-06 12:42:27 +0900

    実行ファイル

---
+++ main.py
@@ -0,0 +1,25 @@
+import numpy as np
+from for_mu_tau_test import CalcRHNParameters
+
+
+def calcallparameters(t12, t23, t13, dm2, Dm2, abLambda, theta, phi, RorD):
+
+    calcNP = CalcRHNParameters(t12, t23, t13, dm2, Dm2,abLambda, theta, phi, RorD)
+    calcNP.calcinputTriAngle()
+    calcNP.calcallNparameters()
+    calcNP.calcRHNMassMatrix()
+    calcNP.diagRHNMassMatrix()
+    calcNP.show_inputall()
+    #calcNP.show_outputall()
+    calcNP.show_output(*["RHNmass[e+20]","U"])
+    return calcNP.output
+
+
+np.set_printoptions(precision=4, suppress=True)
+
+calcallparameters(33.44, 49.2, 8.57, 7.42 * 10 ** (-5), 2.517 * 10 ** (-3), 0.1, 60, 30, "deg")
+calcallparameters(33, 40.5, 8.4, 7.42 * 10 ** (-5), 2.517 * 10 ** (-3), 0.1, 60, 30, "deg")
+calcallparameters(33, 43, 8.5, 7.42 * 10 ** (-5), 2.517 * 10 ** (-3), 0.1, 60, 30, "deg")
+
+
+
